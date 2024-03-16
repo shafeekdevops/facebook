aa411328f1a66dd4db21aa1fa342bde94d74ccb5
shafu424 <162111266+shafu424@users.noreply.github.com>
2024-03-17 05:12:50 +0530

    Delete ServResoc.java

--- ServResoc.java
+++
@@ -1,5 +0,0 @@
-Adding line in master branch to test merging
-Don't forget why you started.
-Adding this line in Master branch
-new line using development branch to test merging bw branches
-Today finish practiceing of github till 3 and you are writing in "master branch"
